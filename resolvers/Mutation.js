7043134f9444b7064b182c3b57d648a393291fe1
Randy <randyarbolaez0@gmail.com>
2020-10-08 19:28:47 -0400

    initial commit 7

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -46,7 +46,7 @@ module.exports = {
           ctx.response.cookie("token", token, {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 365,
-            SameSite:Strict
+            SameSite:"Strict"
           });
 
           return user;
